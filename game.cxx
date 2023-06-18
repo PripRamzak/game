722be6f710015b1d0f4da858e25a7758acd32330
PripRamzak <geru7sha@gmail.com>
2023-06-18 14:05:45 +0300

    add projection and scale matrices

--- game.cxx
+++ game.cxx
@@ -19,11 +19,11 @@ int main(int /*argc*/, char** /*argv*/)
     texture* warrior_run_sprite_sheet = create_texture();
     warrior_run_sprite_sheet->load("./img/warrior_run.png", 6);
     texture* warrior_attack_sprite_sheet = create_texture();
-    warrior_attack_sprite_sheet->load("./img/warrior_run.png", 4);
+    warrior_attack_sprite_sheet->load("./img/warrior_attack.png", 4);
     texture* warrior_run_n_attack_sprite_sheet = create_texture();
     warrior_run_n_attack_sprite_sheet->load("./img/warrior_run&attack.png", 4);
 
-    sprite* warrior_sprite = create_sprite(0.2f, 0.2f, 0.3f, 0.3f);
+    sprite* warrior_sprite = create_sprite(-120.f, 120.f, 288.f, 144.f);
     warrior_sprite->add_texture(warrior_idle_sprite_sheet);
     warrior_sprite->add_texture(warrior_run_sprite_sheet);
     warrior_sprite->add_texture(warrior_attack_sprite_sheet);
@@ -39,21 +39,24 @@ int main(int /*argc*/, char** /*argv*/)
 
     int direction = 0;
 
-    /*glm::mat3 mat_projection = {
-        1.f / static_cast<float>(engine->get_window_width()),
-        0.f,
-        0.f,
-        0.f,
-        1.f / static_cast<float>(engine->get_window_height()),
-        0.f,
-        0.f,
-        0.f,
-        1.f
-    };*/
+    glm::mat3 mat_to_ndc_coordinates{ 1.f };
+    mat_to_ndc_coordinates[0].x =
+        1.f / static_cast<float>(engine->get_window_width());
+    mat_to_ndc_coordinates[1].y =
+        1.f / static_cast<float>(engine->get_window_height());
+
+    glm::mat3 mat_projection{ 1.f };
+    mat_projection[1].y = static_cast<float>(engine->get_window_width()) /
+                          engine->get_window_height();
+
     float last_time        = engine->get_time();
     bool  quit             = false;
     bool  show_menu_window = true;
     event event;
+
+    bool warrior_idle   = true;
+    bool warrior_run    = false;
+    bool warrior_attack = false;
 
     while (!quit)
     {
@@ -63,42 +66,78 @@ int main(int /*argc*/, char** /*argv*/)
             std::cout << event << std::endl;
             if (event == event::up_pressed)
             {
+                warrior_idle = false;
+                warrior_run  = true;
                 warrior_sprite->move(0.f, 0.03f);
             }
             else if (event == event::down_pressed)
             {
+                warrior_idle = false;
+                warrior_run  = true;
                 warrior_sprite->move(0.f, -0.03f);
             }
             else if (event == event::left_pressed)
             {
+                warrior_idle = false;
+                warrior_run  = true;
                 warrior_sprite->move(-0.03f, 0.f);
             }
             else if (event == event::right_pressed)
             {
+                warrior_idle = false;
+                warrior_run  = true;
                 warrior_sprite->move(0.03f, 0.f);
             }
+            else
+            {
+                warrior_idle = true;
+                warrior_run  = false;
+            }
+
+            if (event == event::select_pressed)
+                warrior_attack = true;
+            else
+                warrior_attack = false;
+
             if (event == event::turn_off)
             {
                 quit = true;
                 break;
             }
         }
-        mat_move[2][0] = warrior_sprite->get_current_pos_x();
-        mat_move[2][1] = warrior_sprite->get_current_pos_y();
+
+        mat_move[2].x = warrior_sprite->get_current_pos_x();
+        mat_move[2].y = warrior_sprite->get_current_pos_y();
 
         if (show_menu_window)
             engine->render_menu(show_menu_window);
         else
         {
-            // glm::mat3 mat_result = mat_move * mat_projection;
+            glm::mat3 mat_result =
+                mat_move * mat_projection * mat_to_ndc_coordinates;
+            if (warrior_idle)
+            {
+                if (warrior_attack)
+                    warrior_sprite->set_current_texture(2);
+                else
+                    warrior_sprite->set_current_texture(0);
+            }
+            else if (warrior_run)
+            {
+                if (warrior_attack)
+                    warrior_sprite->set_current_texture(3);
+                else
+                    warrior_sprite->set_current_texture(1);
+            }
+
             engine->render(warrior_vertex_buffer,
                            warrior_index_buffer,
                            warrior_sprite->get_sprite(),
                            0,
-                           &mat_move[0][0]);
+                           &mat_result[0][0]);
             if (engine->get_time() - last_time > 0.15f)
             {
-                warrior_idle_sprite_sheet->next_texture();
+                warrior_sprite->next_sprite();
                 last_time = engine->get_time();
             }
         }
--- sprite.cxx
+++ sprite.cxx
@@ -8,8 +8,8 @@ class game_sprite final : public sprite
     std::vector<texture*> textures;
     vertex_2d             vertices[4];
     int                   current_texture = 0;
-    float                 current_pos_x   = 0.f;
-    float                 current_pos_y   = 0.f;
+    float                 delta_pos_x     = 0.f;
+    float                 delta_pos_y     = 0.f;
 
 public:
     game_sprite(float pos_x, float pos_y, float width, float height)
@@ -32,12 +32,12 @@ public:
     void next_sprite() final { textures[current_texture]->next_texture(); }
     void move(float x, float y) final
     {
-        current_pos_x += x;
-        current_pos_y += y;
-        std::cout << current_pos_x << " " << current_pos_y << std::endl;
+        delta_pos_x += x;
+        delta_pos_y += y;
+        std::cout << delta_pos_x << " " << delta_pos_y << std::endl;
     }
-    float      get_current_pos_x() final { return current_pos_x; }
-    float      get_current_pos_y() final { return current_pos_y; }
+    float      get_current_pos_x() final { return delta_pos_x; }
+    float      get_current_pos_y() final { return delta_pos_y; }
     vertex_2d* get_vertices() final { return vertices; }
     texture*   get_sprite() final { return textures[current_texture]; }
 };
